c261303b7e5d13463b2797290098564f0c0a4eba
Raoni Botelho Sporteman <raonibs@gmail.com>
2022-06-13 11:03:59 -0300

    simple number calc with tests work

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # wikibooks.org-Ruby_Programming-Unit_testing
+https://en.wikibooks.org/wiki/Ruby_Programming/Unit_testing
---
+++ simple_number.rb
@@ -0,0 +1,19 @@
+# File:  simple_number.rb
+
+class SimpleNumber
+
+  def initialize(num)
+    raise unless num.is_a?(Numeric)
+    @x = num
+  end
+
+  def add(y)
+    @x + y
+  end
+
+  def multiply(y)
+    @x * y
+  end
+
+end
+
